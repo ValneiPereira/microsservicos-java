a08bc9e7798635bda1305332d9d22c21b1187c9d
ValneiPereira <valneipereirabarboza@gmail.com>
2021-01-04 18:23:14 -0300

    Feign client

--- hr-payroll/src/main/java/com/superior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/superior/hrpayroll/services/PaymentService.java
@@ -1,15 +1,11 @@
 package com.superior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.superior.hrpayroll.entities.Payment;
 import com.superior.hrpayroll.entities.Worker;
+import com.superior.hrpayroll.feignclientes.WorkerFeignClient;
 
 
 
@@ -17,18 +13,14 @@ import com.superior.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
+			
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 		
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}" , Worker.class, uriVariables);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
